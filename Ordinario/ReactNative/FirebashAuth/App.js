7931ebd97617a863db4dc864d6e003bb436d4a25
Mauricio Salas <202160546@ucc.mx>
2025-10-28 20:43:28 -0600

    Subida de actividades

--- Ordinario/ReactNative/FirebashAuth/App.js
+++ Ordinario/ReactNative/FirebashAuth/App.js
@@ -1,93 +1,113 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
 import { auth } from './firebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
+
 
 export default function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [user, setUser] = useState(null);
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ user, setUser] = useState(null);
 
   const handleLogin = async () => {
     try{
-      const userCredential = signInWithEmailAndPassword(auth, email, password)
-      setUser(userCredential.user);
-    }catch(error){
-      Alert.alert("Login Failed", error.message);
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    setUser(userCredential.user);
+  }
+    catch(error){
+      Alert.alert("Error", error.message);
     }
-
   }
 
   const handleLogout = async () => {
     await auth.signOut();
     setUser(null);
   }
-
+  
   const handleSignUp = async () => {
     try{
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      setUser(userCredential.user);  
-    } catch(error){
-      Alert.alert("Sign Up Failed", error.message);
+      setUser(userCredential.user);
+    }
+    catch(error){
+      Alert.alert("Error", error.message);
     }
   }
 
-  const handleRefresh = () =>{
-    const nuevoAccesToken= user.getIdToken(true);
+  const handleRefresh = async () => {
+    console.log("TOKEN ANTERIOR");
+    console.log("");
+    const nuevoAccesoToken = await user.getIdToken(true);
+    console.log ("TOKEN REGISTRADO");
+    
   }
 
-  return (
-    <View style={styles.container}>
-      {
-        user ? (
-          <>
-            <Text>Welcome, {user.email}</Text>
-            <Button title="Logout" onPress={handleLogout} />
-          </>
-        ):(
-          <>
-            <Text>Authentication with FireBase</Text>
-            <TextInput style={styles.input} 
-            placeholder='email' 
-            value={email} 
-            onChangeText={setEmail} 
-            keyboardType='email-address'>
+return (
+  <View style={styles.container}>
+    {
+      user ? (
+        <>
+        <Text style={styles.texto}>Bienvenido {user.email}</Text>
+        <Button 
+        title="Abandonar Sesión"
+        onPress={handleLogout}
+        />
+        </>
+      ):(
+        <>
+        <Text>Autenticación con firebase</Text>
+        
+        <TextInput
+          style={styles.input}
+          placeholder='correo electronico'
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
 
-            </TextInput>
-            <TextInput style={styles.input} 
-            placeholder='password' 
-            value={password} 
-            onChangeText={setPassword} 
-            secureTextEntry>
+        <TextInput
+          style={styles.input}
+          placeholder='password'
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        
+        <Button
+          title="Acceder"
+          onPress={handleLogin}
+          />
+        <View style={{marginBottom:10}}/> 
+        <Button
+          title="Registrarse"
+          onPress={handleSignUp}
+        />
+        </>
+      )
+    }
+  </View>  
+);
 
-            </TextInput>
-            <Button style = {styles.Button} title='Login' onPress={handleLogin}></Button>
-            <View style={{marginBottom:10}}></View>
-            <Button style = {styles.Button} title='Register' on onPress={handleSignUp}></Button>
-          </>
-        )
-      }
-    </View>
-  );
 }
 
-const styles = StyleSheet.create(
-    {
-      container: {
-      flex: 1,
-      backgroundColor: '#b2e4f3ff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 20,
-    },
-    input:{
-      width: 200,
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginBottom: 10,
-      padding: 5,
-    },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffffff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input:{
+    width: "100%",
+    padding: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#f4f5f4ff",
+    borderRadius: 10,
+    backgroundColor: "#ffffffff"
+  },
+  texto:{
+    fontSize: 26,
+    color: "#f5f5f5ff"
   }
-);
+});
